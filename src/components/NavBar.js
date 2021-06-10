67f5752a94f552bea6489488a90b520921332b30
Bryn Knowles <bryn.knowles@gmail.com>
2021-06-10 14:09:20 -0400

    build nav component

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavBar({ currentUser, setCurrentUser }) {
+    function handleLogout() {
+        // remove the user from state
+        setCurrentUser(null);
+        // remove the token from localStorage
+        localStorage.removeItem("token");
+    }
+
+    return (
+        <header>
+        <div>
+            <Link to="/">Home</Link>
+        </div>
+        <div>
+            {currentUser ? (
+            <>
+                <Link to="/profile">Profile</Link>
+                <button onClick={handleLogout}>Logout</button>
+            </>
+            ) : (
+            <>
+                <Link to="/signup">Signup</Link>
+                <Link to="/login">Login</Link>
+            </>
+            )}
+        </div>
+        </header>
+    );
+}
+
+export default NavBar;
