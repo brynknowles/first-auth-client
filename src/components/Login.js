cc9738703e2e656e9f43710141ff1f55c13dd4d1
Bryn Knowles <bryn.knowles@gmail.com>
2021-06-10 14:13:37 -0400

    build login component

---
+++ src/components/Login.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function Login({ setCurrentUser }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        // TODO: make a fetch request to login the current user
+        const formData = { username, password };
+
+        fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+        })
+        .then((r) => r.json())
+        .then((data) => {
+            // then set that user in state in our App component
+            setCurrentUser(data.user);
+            // save the user's token
+            localStorage.setItem("token", data.token);
+        });
+
+        // and redirect to the home page
+    }
+
+    return (
+        <div>
+        <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <label htmlFor="username">Username</label>
+            <input
+            type="text"
+            id="username"
+            autoComplete="off"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="current-password"
+            />
+            <input type="submit" value="Login" />
+        </form>
+        </div>
+    );
+}
+
+export default Login;
